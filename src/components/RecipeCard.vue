18b695c132e4de18cad997b9614c9b2430d0d362
Steve Rumizen <steverumizen@gmail.com>
2020-01-26 15:21:58 -0700

    Add api call to get selected recipe and steps/ingredients

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -1,5 +1,9 @@
 <template>
-  <article v-bind:id="id" v-bind:style="{backgroundImage: `url(${image})`}">
+  <article
+    @click="handleClick"
+    v-bind:id="id"
+    v-bind:style="{ backgroundImage: `url(${image})` }"
+  >
     <div class="title-gradient">
       <h5>{{ title }}</h5>
     </div>
@@ -7,12 +11,24 @@
 </template>
 
 <script>
+import { store } from "../store/store";
+import { mapActions } from "vuex";
+
 export default {
   name: "RecipeCard",
+  store,
   props: {
     title: String,
     image: String,
     id: String
+  },
+  methods: {
+    ...mapActions({
+      setSelectedRecipe: "setSelectedRecipe"
+    }),
+    handleClick: function() {
+      this.$store.dispatch("setSelectedRecipe", this.id);
+    }
   }
 };
 </script>
@@ -29,29 +45,32 @@ article {
   height: 15rem;
   width: 20rem;
   margin: 1rem;
-  box-shadow: 0 1px 6px 0 rgba(32,33,36,0.28);
-  border-radius: .25rem;
-  transition: all .25s;
+  box-shadow: 0 1px 6px 0 rgba(32, 33, 36, 0.28);
+  border-radius: 0.25rem;
+  transition: all 0.25s;
   &:hover {
     cursor: pointer;
     box-shadow: none;
-    opacity: .8;
+    opacity: 0.8;
   }
 }
 h5 {
   color: white;
   font-weight: 300;
   font-size: 1rem;
-  margin: .75rem;
+  margin: 0.75rem;
   text-align: center;
 }
 .title-gradient {
   height: 100%;
   width: 100%;
-  background: linear-gradient(0deg, rgba(255,255,255,0) 70%, rgba(0,0,0,.8) 100%);
-  border-radius: .25rem;
+  background: linear-gradient(
+    0deg,
+    rgba(255, 255, 255, 0) 70%,
+    rgba(0, 0, 0, 0.8) 100%
+  );
+  border-radius: 0.25rem;
 }
 img {
-
 }
 </style>
